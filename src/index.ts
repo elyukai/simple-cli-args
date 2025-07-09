f8bd9427de901101dad7ee3b938902d87fc001d5
Lukas Obermann <lukas.obermann@icloud.com>
2025-07-09 23:17:10 +0200

    fix: export types from single index export

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ import type { ParsedArguments } from "./parsedOptions.ts"
 import { parse } from "./parser.ts"
 
 export const parseArguments = (options: Arguments): ParsedArguments => parse(options, argv.slice(2))
+
+export type * from "./options.ts"
+export type * from "./parsedOptions.ts"
